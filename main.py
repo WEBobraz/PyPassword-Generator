50fe7fa4b76eb90acac7d126d4b16ae6ce9344c2
WEBobraz <56693534+WEBobraz@users.noreply.github.com>
2023-09-14 19:00:56 +0000

    Eazy Level

--- main.py
+++ main.py
@@ -12,6 +12,35 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+password = ""
+#nr_letters = 4
+for char in range(1, nr_letters + 1):
+    #1 - 4
+
+    # random_char = random.choice(letters)
+    # print(random_char)
+    # N
+    # S
+    # M
+    # R
+
+    # random_char = random.choice(letters)
+    # password += random_char #password = password + random_char
+# print(password)
+  
+    password += random.choice(letters)
+    # M
+    # MH
+    # MHe
+    # MHeq
+
+for char in range(1, nr_symbols + 1):
+    password += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+    password += random.choice(numbers)
+
+print(password)
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
